ed863c3f4f7d7f1851b9fd1a415475c34c8264ed
Christopher Cannon <CCannon@c3-macBookPro.local>
2017-04-25 14:28:01 -0400

    Implement Precision enum

--- Float/Float/Precision.cs
+++ Float/Float/Precision.cs
@@ -1,8 +1,29 @@
 ﻿using System;
 namespace Float
 {
+    /// <summary>
+    /// Enumerates the precision of a floating point conversion
+    /// </summary>
     public enum Precision
     {
-        
+        /// <summary>
+        /// Single precision, 32-bit representation of a number.
+        /// </summary>
+        Single = 0x01;
+
+        /// <summary>
+        /// Half precision, 16-bit representation of a number.
+        /// </summary>
+        Half = 0x02;
+
+        /// <summary>
+        /// Double precision, 64-bit representation of a number.
+        /// </summary>
+        Double = 0x03;
+
+        /// <summary>
+        /// Quad precision, 128-bit represenation of a number.
+        /// </summary>
+        Quad = 0x04;
     }
 }
