d9f507bc56eaaf8910591a2fc863e837bfce9d0b
Christopher Cannon <CCannon@c3-macBookPro.local>
2017-04-25 14:36:10 -0400

    Implement no-arg constructor of fpn

--- Float/Float/FloatingPointNumber.cs
+++ Float/Float/FloatingPointNumber.cs
@@ -6,8 +6,23 @@ namespace Float
     /// </summary>
     public class FloatingPointNumber
     {
+        private Precision precision;
+        private byte[] pBytes;
+        private byte[] qBytes;
+        private byte signBit;
+        private string floatingPointValue;
+
+
         public FloatingPointNumber()
         {
+            precision = Precision.Half;
+            pBytes = new byte[1];
+            pBytes[0] = 0x0;
+            qBytes = new byte[1];
+            qBytes[0] = 0x0;
+            signBit = 0x0;
+
+            floatingPointValue = "0";
         }
     }
 }
