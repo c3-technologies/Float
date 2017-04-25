ccdbe5562bf11e0ce0c37bc68fc03e374d755c0e
Christopher Cannon <CCannon@c3-macBookPro.local>
2017-04-25 14:34:17 -0400

    Fix enum to proper format

--- Float/Float/FloatingPointNumber.cs
+++ Float/Float/FloatingPointNumber.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 namespace Float
 {
+    /// <summary>
+    /// Represents a floating-point number in accordance with IEEE specification 754.
+    /// </summary>
     public class FloatingPointNumber
     {
         public FloatingPointNumber()
--- Float/Float/Precision.cs
+++ Float/Float/Precision.cs
@@ -9,21 +9,21 @@ namespace Float
         /// <summary>
         /// Single precision, 32-bit representation of a number.
         /// </summary>
-        Single = 0x01;
+        Single = 0x01,
 
         /// <summary>
         /// Half precision, 16-bit representation of a number.
         /// </summary>
-        Half = 0x02;
+        Half = 0x02,
 
         /// <summary>
         /// Double precision, 64-bit representation of a number.
         /// </summary>
-        Double = 0x03;
+        Double = 0x03,
 
         /// <summary>
         /// Quad precision, 128-bit represenation of a number.
         /// </summary>
-        Quad = 0x04;
+        Quad = 0x04
     }
 }
