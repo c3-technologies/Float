7178489662846ec33716fa2a8dc4384c843eea0a
Christopher Cannon <CCannon@c3-macBookPro.local>
2017-04-25 14:40:36 -0400

    Implement argument constructor of FloatingPointNumber

--- Float/Float/FloatingPointNumber.cs
+++ Float/Float/FloatingPointNumber.cs
@@ -24,5 +24,46 @@ namespace Float
 
             floatingPointValue = "0";
         }
+
+        public FloatingPointNumber(Precision precision, byte[] pBytes, byte[] qBytes, byte signBit){
+            this.pBytes = pBytes;
+            this.qBytes = qBytes;
+            this.signBit = signBit;
+
+            switch(precision){
+                case Precision.Single:
+                    _initializeSinglePrecision();
+                    break;
+                case Precision.Half:
+                    _initializeHalfPrecision();
+                    break;
+                case Precision.Double:
+                    _initializeDoublePrecision();
+                    break;
+                default:
+                    _initializeQuadPrecision();
+                    break;
+            }
+        }
+
+        private void _initializeQuadPrecision()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _initializeDoublePrecision()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _initializeHalfPrecision()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _initializeSinglePrecision()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
